088bf46a9e45cf8ab2678f9f5662a5e12dc4a36b
Jaakko Heusala <jheusala@iki.fi>
2019-08-31 22:16:54 +0300

    Added PromiseUtils.isPromise()

--- src/PromiseUtils.js
+++ src/PromiseUtils.js
@@ -5,17 +5,27 @@
 class PromiseUtils {
 
     /**
+     * Returns true if value is a promise.
+     *
+     * @param value {*}
+     * @returns {boolean}
+     */
+    static isPromise (value) {
+        return !!(value && value.then);
+    }
+
+    /**
      *
      * @param value {Promise|*}
      * @returns {Promise}
      */
     static when (value) {
 
-        if (value && value.then) {
+        if (PromiseUtils.isPromise(value)) {
             return value;
         }
 
-        return new Promise(resolve => resolve(value));
+        return Promise.resolve(value);
     }
 
 }
