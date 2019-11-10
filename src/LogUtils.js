aa87177bf9daee8542854fd880fa9f94d4ce6715
Jaakko Heusala <jheusala@iki.fi>
2019-11-10 12:54:57 +0200

    Fixed circular reference problem with LogUtils.getAsString()

--- src/LogUtils.js
+++ src/LogUtils.js
@@ -60,7 +60,24 @@ export class LogUtils {
 
         try {
 
-            return JSON.stringify(value);
+            const knownObjects = [];
+
+            return JSON.stringify(value, (key, value) => {
+
+                if (!_.isObject(value)) {
+                    return value;
+                }
+
+                const index = knownObjects.indexOf(value);
+                if (index < 0) {
+                    knownObjects.push(value);
+                    return value;
+                }
+
+                // It's circular reference.
+                return {'$ref': `circular#${index}`};
+
+            });
 
         } catch (err) {
 
