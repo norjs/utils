7ce96e688bf822dd029a17172e24fd9f184afa65
Jaakko Heusala <jheusala@iki.fi>
2019-11-29 16:52:37 +0200

    New DateUtils

---
+++ Date.js
@@ -0,0 +1,2 @@
+export * from './src/DateUtils.js';
+export {default} from './src/DateUtils.js';
---
+++ src/DateUtils.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+import LogUtils from "./LogUtils";
+
+/**
+ *
+ */
+export class DateUtils {
+
+    /**
+     *
+     * @param value {Date|number|string}
+     * @returns {string}
+     */
+    parseToString (value) {
+
+        if ( value && value instanceof Date ) {
+            return value.toISOString();
+        }
+
+        if ( _.isNumber(value) ) {
+            return new Date(value).toISOString();
+        }
+
+        if ( _.isString(value) ) {
+            return value;
+        }
+
+        throw new TypeError(`DateUtils.parseToString(): Could not parse to date string: ${LogUtils.getAsString(value)}`);
+
+    }
+
+}
+
+// Exports
+export default DateUtils;
